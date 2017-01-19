b864981d96edffb5fb66df4c1e710fdb6d2823c1
amcababat <amcababat@ucdavis.edu>
2017-01-19 13:24:15 -0800

    background change

--- studio1/script.js
+++ studio1/script.js
@@ -1,6 +1,6 @@
 function setup() {
     var myCanvas = createCanvas(800, 250);
-background(71,118, 155);
+background(131,185, 224);
     myCanvas.parent('mySketch');
 }
 
@@ -15,6 +15,6 @@ function draw() {
 vertex((mouseX+20), mouseY);
 vertex(mouseX, (mouseY+20));
 vertex((mouseX-20), mouseY);
-endShape();    
+endShape();
     }
   }
